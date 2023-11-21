251a35ec7d82f03151aeec84fc2c44e85cceb16b
calaxo <36140542+calaxo@users.noreply.github.com>
2023-11-22 00:17:42 +0100

    22

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://bv.univ-poitiers.fr/access/content/user/acalen01/',
+  base: 'https://bv.univ-poitiers.fr/access/content/user/acalen01/portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
